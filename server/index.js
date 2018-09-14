dab224b11a3e030dcc44e8f63c8b3339ad4b28f4
Gareth Lowrie <gareth.lowrie@communicatorcorp.com>
2018-09-14 16:04:35 +0100

    Removed new lines

--- server/index.js
+++ server/index.js
@@ -9,10 +9,5 @@ mongoose.connect(keys.mongoURI); // Pass the DB URI to the connection function
 const app = express();
 require('./routes/authRoutes')(app); // Immediately invoke the function exported by the passport
 
-
-
-
-
-
 const PORT = process.env.PORT || 5000; // Heroku will inject the port as an environment variable
 app.listen(PORT); // Start the express server and listen for incoming traffic on the specific port
